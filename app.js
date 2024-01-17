28d34e29d2b1c0db23788f062fcd5f60ad568a72
AliReza Ghasemi <mohali1381@gmail.com>
2024-01-17 16:21:32 +0330

    make GameBoard class from table and some refactoring

--- app.js
+++ app.js
@@ -1,44 +1,79 @@
-let table = [
-    ["_", "_", "_"],
-    ["_", "_", "_"],
-    ["_", "_", "_"]
-]
+const HUMAN = "X";
+const COMPUTER = "O";
+const EMPTY = "_";
 
-var HUMAN = "X";
-var COMPUTER = "O";
-var EMPTY = "_";
+class GameBoard {
+    table;
+    turn;
 
-// printing game table
-function printTable(table) {
-    let outputText = "";
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            outputText += table[i][j];
-            if (j < 2)
-                outputText += " ";
-        }
-        outputText += "\n"
+    constructor(inTable, inTurn) {
+        this.table = [
+            ["_", "_", "_"],
+            ["_", "_", "_"],
+            ["_", "_", "_"]
+        ]
+
+        if (inTable)
+            for (let i = 0; i < 3; i++)
+                for (let j = 0; j < 3; j++)
+                    this.table[i][j] = inTable[i][j];
+
+        if (inTurn)
+            this.turn = inTurn;
+        else
+            this.turn = HUMAN;
     }
-    console.log(outputText);
-}
-
-// set game table with player symbol in index x,y
-function setTableAtXYFrom(table, x, y, player) {
-    if (x > 2 || y > 2 || x < 0 || y < 0)
-        throw "index out of range!";
-    if (table[x][y] == EMPTY)
-        table[x][y] = player;
-    else
-        throw "you can not select filled cell";
+    // printing game table
+    printTable() {
+        let outputText = "";
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                outputText += this.table[i][j];
+                if (j < 2)
+                    outputText += " ";
+            }
+            outputText += "\n"
+        }
+        console.log(outputText);
+    }
+    // set game table with player symbol in index x,y
+    setTableAtXY(x, y) {
+        let isDone = false;
+        if (x == null || y == null)
+            throw "x or y parameters is missing!";
+        if (x > 2 || y > 2 || x < 0 || y < 0)
+            throw "index out of range!";
+        if (this.table[x][y] == EMPTY) {
+            this.table[x][y] = this.getTurn();
+            isDone = true;
+        }
+        else
+            throw "you can not select filled cell!";
+        return isDone;
+    }
+    // update turn between HUMAN and COMPUTER
+    ChangeTurn() {
+        if (this.turn == HUMAN)
+            this.turn = COMPUTER
+        else
+            this.turn = HUMAN
+    }
+    getTurn() {
+        return this.turn;
+    }
 }
 
 //test area
-console.log("turn 1:");
-printTable(table);
-console.log("turn 2:");
-setTableAtXYFrom(table, 1, 1, HUMAN);
-printTable(table);
-console.log("turn 3:");
-setTableAtXYFrom(table, 1, 2, COMPUTER);
-printTable(table);
-console.log("end");
+let board = new GameBoard(null, null);
+board.printTable();
+console.log("turn: " + board.getTurn());
+board.setTableAtXY(1, 1);
+board.printTable();
+board.ChangeTurn();
+console.log("turn: " + board.getTurn());
+board.setTableAtXY(2, 2);
+board.printTable();
+board.ChangeTurn();
+console.log("turn: " + board.getTurn());
+board.setTableAtXY(0, 0);
+board.printTable();
