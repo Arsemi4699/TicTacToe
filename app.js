f8a742e986d11654938c2e57f06a7522e4982ce7
AliReza Ghasemi <mohali1381@gmail.com>
2024-01-17 17:47:50 +0330

    add simplePlay func to make a playable game human against random bot without check winners

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const HUMAN = "X";
-const COMPUTER = "O";
+const prompt = require('prompt-sync')();
+var HUMAN = "X";
+var COMPUTER = "O";
 const EMPTY = "_";
 
 class GameBoard {
@@ -8,9 +9,9 @@ class GameBoard {
 
     constructor(inTable, inTurn) {
         this.table = [
-            ["_", "_", "_"],
-            ["_", "_", "_"],
-            ["_", "_", "_"]
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]
         ]
 
         if (inTable)
@@ -21,7 +22,7 @@ class GameBoard {
         if (inTurn)
             this.turn = inTurn;
         else
-            this.turn = HUMAN;
+            this.turn = "X";
     }
     // printing game table
     printTable() {
@@ -36,18 +37,17 @@ class GameBoard {
         }
         console.log(outputText);
     }
-    // set game table with player symbol in index x,y
-    setTableAtXY(x, y) {
+    // set game table with player symbol in index r,c
+    setTableAtRC(r, c) {
         let isDone = false;
-        if (x == null || y == null)
-            throw "x or y parameters is missing!";
-        if (x > 2 || y > 2 || x < 0 || y < 0)
+        if (r == null || c == null)
+            throw "row or col parameters is missing!";
+        if (r > 2 || c > 2 || r < 0 || c < 0)
             throw "index out of range!";
-        if (this.table[x][y] == EMPTY) {
-            this.table[x][y] = this.getTurn();
+        if (this.table[r][c] == EMPTY) {
+            this.table[r][c] = this.getTurn();
             isDone = true;
-        }
-        else
+        } else
             throw "you can not select filled cell!";
         return isDone;
     }
@@ -58,22 +58,66 @@ class GameBoard {
         else
             this.turn = HUMAN
     }
+    // returns turn that is HUMAN or COMPUTER value
     getTurn() {
         return this.turn;
+    }
+    // checks that game is over or not
+    isTableFull() {
+        for (let i = 0; i < 3; i++)
+            for (let j = 0; j < 3; j++)
+                if (this.table[i][j] == EMPTY)
+                    return false;
+        return true;
+    }
+}
+
+
+function getHUMANTarget() {
+    let targetText = prompt("enter your target cell position (row col): ", "0 0");
+    let targetPos = targetText.split(" ").map(Number);
+    return targetPos;
+}
+
+function getCOMPUTERTarget() {
+    let posRow = Math.floor(Math.random() * 3);
+    let posCol = Math.floor(Math.random() * 3);
+    return [posRow, posCol];
+}
+
+function simplePlay() {
+    let humanSymb = prompt("enter symbol you want to play (X or O): ", HUMAN);
+    if (humanSymb == "X" || humanSymb == "O") {
+        if (humanSymb != HUMAN) {
+            COMPUTER = HUMAN;
+            HUMAN = humanSymb;
+        }
+    } else
+        throw "Wrong Input!";
+
+    let board = new GameBoard(null, null);
+    while (!board.isTableFull()) {
+        let targetPos = [0, 0];
+        let currentTurn = board.getTurn();
+        console.log("turn: " + currentTurn);
+        if (currentTurn == HUMAN) {
+            targetPos = getHUMANTarget();
+        }
+        else {
+            targetPos = getCOMPUTERTarget();
+        }
+        try {
+            if (board.setTableAtRC(targetPos[0], targetPos[1]) == true) {
+                board.printTable();
+                board.ChangeTurn();
+            }
+        }
+        catch (er) {
+            if (currentTurn == HUMAN)
+                console.log(er);
+        }
     }
 }
 
 //test area
-let board = new GameBoard(null, null);
-board.printTable();
-console.log("turn: " + board.getTurn());
-board.setTableAtXY(1, 1);
-board.printTable();
-board.ChangeTurn();
-console.log("turn: " + board.getTurn());
-board.setTableAtXY(2, 2);
-board.printTable();
-board.ChangeTurn();
-console.log("turn: " + board.getTurn());
-board.setTableAtXY(0, 0);
-board.printTable();
+simplePlay();
